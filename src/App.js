c95bfa11a6ed672ed12c485f8c557e59c02f6f00
minha0817 <minhakk24@gmail.com>
2023-02-21 14:16:28 -0800

    Add style for navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MyCart from "./components/MyCart";
 import Nav from "./components/Nav";
 import NewProduct from "./components/NewProduct";
 import { Outlet } from "react-router-dom";
+import "./index.css";
 
 function App() {
   return (
--- src/components/nav.js
+++ src/components/nav.js
@@ -5,27 +5,26 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
-      <div className="nav--left">
-        <Link to={"/"}>
-          <FiShoppingBag />
-          <span>Shoppy</span>
-        </Link>
-      </div>
-      <div className="nav--right">
+    <header className=" flex justify-between mx-auto border-b border-gray-300 p-2">
+      <Link to={"/"} className="flex items-center text-4xl text-brand">
+        <FiShoppingBag className="flex items-center" />
+        <span>Shoppy</span>
+      </Link>
+
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to={"/products"}>
           <span>Products</span>
         </Link>
         <Link to={"/carts"}>
           <span>Carts</span>
         </Link>
-        <Link to={"/products/new"}>
+        <Link to={"/products/new"} className="text-2xl">
           <GrEdit />
         </Link>
         <Link to={"/login"}>
           <span>Login</span>
         </Link>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
