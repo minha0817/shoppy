ca80f7f80ef902415ca5ed9efeefa28273a03696
minha0817 <minhakk24@gmail.com>
2023-02-24 15:58:47 -0800

    Updated images file in public

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Products from "../components/Products";
+import Banner from "./Banner";
 
 
 export default function Home() {
   return (
     <section>
-      
+      <Banner />
       <Products />
     </section>
   );
