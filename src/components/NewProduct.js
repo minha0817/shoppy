3589752ae2a9211eb6bef101a9505b0facaa858a
minha0817 <minhakk24@gmail.com>
2023-02-24 12:42:14 -0800

    Add UI for newproducts

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -36,23 +36,21 @@ export default function NewProduct() {
   };
 
   return (
-    <section>
-      <h2>Add new product</h2>
-      {success && <p>{success}</p>}
-      {file && <img src={URL.createObjectURL(file)} alt="local file" />}
-      <form onSubmit={handleSubmit} className=" flex flex-col p-2">
+    <section className="w-full text-center">
+      <h2 className="text-2xl font-bold my-4">Add new product</h2>
+      {success && <p className="my-2">{success}</p>}
+      {file && <img className="w-96 mx-auto mb-2" src={URL.createObjectURL(file)} alt="local file" />}
+      <form onSubmit={handleSubmit} className=" flex flex-col px-12">
         <input
           type="file"
           accept="/image/*"
           name="file"
           onChange={handleChange}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-1"
         />
         <input
           type="text"
           name="title"
           value={product.title ?? ""}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-1"
           placeholder="Title of product"
           required
           onChange={handleChange}
@@ -61,7 +59,6 @@ export default function NewProduct() {
           type="number"
           name="price"
           value={product.price ?? ""}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-1"
           placeholder="Price"
           required
           onChange={handleChange}
@@ -70,7 +67,6 @@ export default function NewProduct() {
           type="text"
           name="category"
           value={product.category ?? ""}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-1"
           placeholder="Category"
           onChange={handleChange}
           required
@@ -79,7 +75,6 @@ export default function NewProduct() {
           type="text"
           name="description"
           value={product.description ?? ""}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-1"
           placeholder="Description"
           onChange={handleChange}
           required
@@ -88,7 +83,6 @@ export default function NewProduct() {
           type="text"
           name="options"
           value={product.options ?? ""}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-1"
           placeholder="Option(Divide with ,)"
           onChange={handleChange}
           required
