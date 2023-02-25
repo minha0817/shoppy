f1821d3f07e9532c3609a476e7bddc631ffa97ca
minha0817 <minhakk24@gmail.com>
2023-02-24 21:48:20 -0800

    Add products on AllProduct page

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,7 +1,10 @@
 import React from "react";
-
-export default function AllProducts () {
+import Products from "./Products";
+export default function AllProducts() {
   return (
-    <h1>All products</h1>
-  )
+    <section className="w-full">
+      <h2 className="text-2xl font-bold my-4 text-center">All products</h2>
+      <Products />
+    </section>
+  );
 }
